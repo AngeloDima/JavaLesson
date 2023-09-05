6d9b5641de25d4f8f6c7aead26744a6a26cd1165
AngeloDima <Angelodimauro91@gmail.com>
2023-09-05 02:35:39 +0200

    lesson1

---
+++ Main.java
@@ -0,0 +1,16 @@
+package Lesson1_dati;
+
+public class Main {
+    public static void main(String[] args) {
+        // dati
+        // String
+        // Boolean
+        // short
+        // int
+        // long
+        // byte
+        // double
+        // float
+        // char
+    }
+}
